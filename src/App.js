a8f8e666bf0bd84c8d92819d1801c4999f8ed0f6
CASTAN <christophecastan4@gmail.com>
2020-12-21 17:39:46 +0100

    Add MovieCard component with CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Header title="Ma bibliothèque de films" />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
     </div>
   );
 }
